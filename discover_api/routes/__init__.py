acd0c0f40fb078fdc542b52d01d984c47d68f6f1
Hassan Aziz <hassanaziz0012@gmail.com>
2026-07-30 18:04:14 +0500

    Register discover router in API routes package

--- discover_api/routes/__init__.py
+++ discover_api/routes/__init__.py
@@ -4,6 +4,7 @@ from .outliers import router as outliers_router
 from .videos import router as videos_router
 from .lists import router as lists_router
 from .thumbnails import router as thumbnails_router
+from .discover import router as discover_router
 
 router = APIRouter()
 router.include_router(creators_router)
@@ -11,3 +12,4 @@ router.include_router(outliers_router)
 router.include_router(videos_router)
 router.include_router(lists_router)
 router.include_router(thumbnails_router)
+router.include_router(discover_router)
